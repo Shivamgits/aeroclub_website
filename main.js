c6440f357db1eed4aa923e79fd5949623e6684d1
shivam singh <92594066+Shivamgits@users.noreply.github.com>
2023-01-16 15:29:45 +0530

    1

---
+++ main.js
@@ -0,0 +1,66 @@
+//initialize WOW JS
+new WOW().init();
+
+//initialize Swippers
+const swiper = new Swiper('.swiper', {
+ 
+  
+  autoplay: {
+    loop: true,
+    delay:3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+    stopOnLastSlide: false,
+  },
+});
+
+var aboutSwiper = new Swiper(".aboutSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is <= 599px
+    599: {
+      slidesPerView: 2,
+      spaceBetweenSlides: 50
+    },
+  }	
+  
+  
+});
+var planeSwiper = new Swiper(".planeSwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+//Get The Elements
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu-btn");
+
+//Toggle the navbar menu on click Open/Close 
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle('nav-toggle');
+});
+
+//Get the current year and add it into the HTML
+document.querySelector(".year").innerHTML = new Date().getFullYear();
+
+
